30eabbd1222e0d7b81aa3c4f51844f984a78ca94
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-01-24 15:16:11 +0100

    Update flac.sh

--- flac.sh
+++ flac.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #bash script to convert flac to mp3 using ffmpeg and "parallel"
 
-parallel ffmpeg -i {} -qscale:a 0 {.}.mp3 ::: ./*.flac # convert flac to mp3
+parallel ffmpeg -i {} -qscale:a 0 {.}.mp3 ::: ./*.flac
 
-# move flac to flac dir
 mkdir flac
 mv *.flac ./flac
 
-# move mp3 to mp3 dir
 mkdir mp3
 mv *.mp3 ./mp3
 
@@ -17,15 +15,18 @@ mkdir info
 mv *.jpg ./info
 mv *.pnf ./info
 mv *.png ./info
-mv *.log ./info
+
 
 # delete unwanted stuff
 rm *.m3u
 rm *.cue
+rm *.pls
 rm *.url
 rm *.nfo
 rm *.txt
 rm *.html
+rm *.htm
+mv *.log
 
 # add flac folder to zip file
 7z a flac.zip flac\
