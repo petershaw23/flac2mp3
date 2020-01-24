7ac145adf7d0c8c206c4e554fe3102a69ee8183c
petershaw23 <44604841+petershaw23@users.noreply.github.com>
2020-01-24 14:26:08 +0100

    Update flac.sh

--- flac.sh
+++ flac.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-
-# version1:
-##for a in ./*.flac; do
-#version1b:
-#find -type f -name "*.flac" -print0 | while read -d $'\0' a; do
-#  < /dev/null ffmpeg -i "$a" -qscale:a 0 "${a[@]/%flac/mp3}"
-#done
-
-#version2, needs package "parallel":
+#bash script to convert flac to mp3 using ffmpeg and "parallel"
 parallel ffmpeg -i {} -qscale:a 0 {.}.mp3 ::: ./*.flac
+mkdir flac
+mv *.flac ./flac2
+mkdir mp3
+mv *.mp3 ./mp3
